78484bed5c47da4bb9f1897ba57ca1aeddfeae94
Official-User-Shabab <officialshabab24@gmail.com>
2024-07-21 19:50:15 +0100

    Update main.py

--- Picking the right A Levels/main.py
+++ Picking the right A Levels/main.py
@@ -1,1 +1,6 @@
-
+# There are many A Levels
+# + perhaps each is a method of a class? or maybe a class themselves?
+# There are many careers/workpaths
+# + possibly sort these out as individual functions for each
+# There are many industries
+# + maybe sort out through if else statements (industries are maximum 7)
