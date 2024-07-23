c9110a687a6e91ec72df8f58b7f3cae5f3b7af42
Official-User-Shabab <officialshabab24@gmail.com>
2024-07-23 12:57:03 +0100

    Update main.py

--- Picking the right A Levels/main.py
+++ Picking the right A Levels/main.py
@@ -4,3 +4,13 @@
 # + possibly sort these out as individual functions for each
 # There are many industries
 # + maybe sort out through if else statements (industries are maximum 7)
+
+# Industries:
+# Health Care
+# Information Technology, Financials, Communication Services, Energy, Utilities
+# Consumer Discretionary, Consumer Staples, Real Estate, Materials, Industrials
+# Shortened to:
+# Engineering
+# Health
+# Finances and Production
+# Law and Literature
